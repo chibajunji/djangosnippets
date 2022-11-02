b13e4fa78d209abfa9a6339ccb4ae70e036d95c7
chibajunji <chiba@sk2.so-net.ne.jp>
2022-11-02 12:36:19 +0900

    1.2.3 Hello Worldの表示（ルーティングの設定）

--- snippets/tests.py
+++ snippets/tests.py
@@ -4,12 +4,10 @@ from snippets.views import top
 
 class TopPageViewTest(TestCase):
     def test_top_returns_200(self):
-        request = HttpRequest()
-        response = top(request)
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_top_returns_expected_content(self):
-        request = HttpRequest()
-        response = top(request)
+        response = self.client.get('/')
         self.assertEqual(response.content, b'Hello World')
 
