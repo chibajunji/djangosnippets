714d0f2c01911606ef74a7eb7074d4adb5f5d32e
chibajunji <chiba@sk2.so-net.ne.jp>
2022-11-02 17:24:25 +0900

    1.4.1 HTMLの生成

--- snippets/tests.py
+++ snippets/tests.py
@@ -4,6 +4,15 @@ from django.urls import resolve
 
 from snippets.views import top, snippet_new, snippet_edit, snippet_detail
 
+class TopPageTest(TestCase):
+    def test_top_page_returns_200_and_expected_title(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Djangoスニペット', status_code=200)
+
+    def test_top_page_uses_expected_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'snippets/top.html')
+
 class CreateSnippetTest(TestCase):
     def test_should_resolve_snippet_new(self):
         found = resolve('/snippets/new/')
@@ -18,13 +27,3 @@ class EditSnippetTest(TestCase):
     def test_should_resolve_snippet_edit(self):
         found = resolve('/snippets/1/edit/')
         self.assertEqual(snippet_edit, found.func)
-
-class TopPageViewTest(TestCase):
-    def test_top_returns_200(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_top_returns_expected_content(self):
-        response = self.client.get('/')
-        self.assertEqual(response.content, b'Hello World')
-
