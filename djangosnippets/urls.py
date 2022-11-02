a7ad70998e9fb4d0ab7763d55fda28606af1cf3e
chibajunji <chiba@sk2.so-net.ne.jp>
2022-11-02 14:57:07 +0900

    1.2.4 ルーティングの設定

--- djangosnippets/urls.py
+++ djangosnippets/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from snippets.views import top
 
 urlpatterns = [
     path('', top, name='top'),
+    path('snippets/', include('snippets.urls')),  # snippets/urls.pyの読込
     path('admin/', admin.site.urls),
 ]
