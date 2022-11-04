331924f92cc1f2181e9655329a6058045b5789fc
chibajunji <chiba@sk2.so-net.ne.jp>
2022-11-04 18:27:26 +0900

    1.6.2 認証用クラスページビューを利用する

--- djangosnippets/urls.py
+++ djangosnippets/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+
 from snippets.views import top
 
 urlpatterns = [
     path('', top, name='top'),
     path('snippets/', include('snippets.urls')),  # snippets/urls.pyの読込
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
